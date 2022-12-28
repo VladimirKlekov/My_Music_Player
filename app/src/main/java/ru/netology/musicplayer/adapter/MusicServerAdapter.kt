3313fc1db5437eb7c54b055a79978cea0e27a8c5
Vladimir Klekov <vladimirklekovgm@gmail.com>
2022-12-28 22:12:01 +0300

    исправил ошибку при получении json

--- app/src/main/java/ru/netology/musicplayer/adapter/MusicServerAdapter.kt
+++ app/src/main/java/ru/netology/musicplayer/adapter/MusicServerAdapter.kt
@@ -7,12 +7,12 @@ import androidx.recyclerview.widget.RecyclerView
 import ru.netology.musicplayer.databinding.MusicServerViewBinding
 
 
-class MusicServerAdapter(private val context: Context, private val musicListServer: ArrayList<MusicJson>) :
+class MusicServerAdapter(private val context: Context, private val musicListServer: ArrayList<Track>) :
     RecyclerView.Adapter<MusicServerAdapter.MyHolder>() {
     /** для управления music_view */
     class MyHolder(binding: MusicServerViewBinding) : RecyclerView.ViewHolder(binding.root) {
-        val nameSong = binding.nameSongSMV
-        val title = binding.titleSMV
+        val nameSong = binding.nameSongSMV//file из json
+
         val root = binding.root
     }
 
