b120137269efa8c3cb3dcb05b26cb61f632034bb
Vladimir Klekov <vladimirklekovgm@gmail.com>
2023-01-04 11:00:18 +0300

    доработал seek bar.

--- app/src/main/java/ru/netology/musicplayer/PlayerActivity.kt
+++ app/src/main/java/ru/netology/musicplayer/PlayerActivity.kt
@@ -175,6 +175,7 @@ class PlayerActivity : AppCompatActivity(), ServiceConnection {
        val binder = service as MusicService.MyBinder
         musicService = binder.currentService()
         createMediaPlayer()
+        musicService!!.seekBarSetup()
 
     }
 
